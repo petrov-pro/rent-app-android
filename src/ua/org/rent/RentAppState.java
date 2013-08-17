374e2d195119e404722d4f780f4fc8f18ec3d37e
Beathan <beathan.n@gmail.com>
2013-08-17 10:17:03 +0300

    Добавлнен state applcation и DB singleton

---
+++ src/ua/org/rent/RentAppState.java
@@ -0,0 +1,123 @@
+package ua.org.rent;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 17.08.13
+ * Time: 10:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+
+
+
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
+import android.content.Context;
+import android.app.Application;
+import android.util.Log;
+
+
+
+public class RentAppState extends Application {
+    private static RentAppState sInstance;
+    private static Context mContext;
+    private static int counter=0;
+    private static int tabHeight = 0;
+
+    public static RentAppState getAppInstance() {
+        if (sInstance == null) {
+            throw new RuntimeException("The application has not been initialized.");
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+        sInstance = this;
+        mContext = this.getApplicationContext();
+        trustEveryone();
+
+
+    }
+
+
+    //stub
+    public int getCouner(){
+        if(counter>10) counter=0;
+        return counter+=3;
+    }
+
+
+    public static void setTabHeight(int tabHeight) {
+        tabHeight = tabHeight;
+    }
+
+    public static int getTabHeight() {
+        return tabHeight;
+    }
+
+
+    public static Context getContext() {
+        return mContext;
+    }
+
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[] { new X509TrustManager() {
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {}
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {}
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+            }
+            }, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void cleanUserData() {
+
+        Log.d("SYNC", "Clean Up situation");
+    }
+
+}
---
+++ src/ua/org/rent/library/DB.java
@@ -0,0 +1,140 @@
+
+package ua.org.rent.library;
+
+import android.content.Context;
+import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import ua.org.rent.R;
+import ua.org.rent.RentAppState;
+
+public class DB extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "patient.sqlite";
+
+    public static final String TABLE_IMAGES			= "image_cache";
+    public static final String TABLE_MEAL_CATEGORY	= "meal_category";
+    public static final String TABLE_MEAL				= "meal";
+    public static final String TABLE_EVENT_MEAL_TMP	= "event_Meal_Tmp";
+
+    private static final int DATABASE_VERSION = 12;
+    private volatile static DB sInstance;
+    private final Context mContext;
+
+    public static SQLiteDatabase getDb() {
+        if (sInstance == null) {
+            sInstance = new DB(RentAppState.getAppInstance());
+        }
+        return sInstance.getWritableDatabase();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        close();
+        super.finalize();
+    }
+
+    private DB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createTables(db);
+    }
+
+    private void createTables(SQLiteDatabase db) {
+        final String[] items = mContext.getResources().getStringArray(R.array.DDL);
+        for (String ddl: items) {
+            db.execSQL(ddl);
+        }
+
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Context context = RentAppState.getAppInstance();
+        Editor editor = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE).edit();
+        editor.putString("access_token", null);
+        editor.putBoolean("isBuildDbFromLocalAssets", true);
+        editor.commit();
+
+        deleteTables(db);
+        createTables(db);
+    }
+
+    private void deleteTables(SQLiteDatabase db) {
+        Cursor cursor = db.query("sqlite_master", new String[]{"name"}, "type = ? AND NOT name = ? AND NOT name = ?", new String[]{"table","sqlite_sequence","android_metadata",}, null, null, null);
+        while(cursor.moveToNext()) {
+            Log.i("DELETED","Table: "+cursor.getString(0));
+            db.execSQL("DROP TABLE " + cursor.getString(0));
+        }
+        cursor.close();
+
+    }
+
+
+
+
+    public static void cleanAllEvents() {
+        final SQLiteDatabase db = getDb();
+        final Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type = 'table' " +
+                "AND name LIKE('event%') ORDER BY name DESC", null);
+        try {
+            while (c.moveToNext()) {
+                db.delete(c.getString(0), null, null);
+            }
+        } finally {
+            c.close();
+        }
+    }
+
+    public static void removeDeliveredEvents() {
+
+        if (1==1)return;
+       /*
+        Cursor cursor = DB.getDb().query(Event.TABLE_NAME, new String[]{"max (timestamp)"}, "is_delivered = 1 AND type = ?", new String[]{String.valueOf(Const.BOX_BOLUS)}, null, null, null);
+        Log.d("Cleaning delivered items","type "+Event.TABLE_NAME);
+        if(cursor.moveToFirst()) {
+            long maxtTimestamp = cursor.getLong(0);
+            if(maxtTimestamp > 0) DB.getDb().delete("event", "is_delivered = 1 AND timestamp <= ? AND type <> ? ", new String[]{String.valueOf(maxtTimestamp),String.valueOf(Const.BOX_BOLUS)});
+            long bolusMaxTmp = System.currentTimeMillis() - Format.hoursToMilliseconds(Settings.get().getSyncSettings().getUserProfile().getInt(UserValues.ai_time)) - Format.hoursToMilliseconds(3);
+            DB.getDb().delete("event", "is_delivered = 1 AND type = ? AND timestamp < ?", new String[]{String.valueOf(Const.BOX_BOLUS), String.valueOf(bolusMaxTmp)});
+        }
+
+        cursor.close();    */
+    }
+
+
+
+
+    public static final int getInt(final String sql, int columnIndex) {
+        final Cursor c = getDb().rawQuery(sql, null);
+        try {
+            if (!c.moveToFirst()) return -1;
+            return c.getInt(columnIndex);
+        } finally {
+            c.close();
+        }
+    }
+
+    public static final String getString(final Cursor c, final String fieldName) {
+        return c.getString(c.getColumnIndex(fieldName));
+    }
+
+    public static final int getInt(final Cursor c, final String fieldName) {
+        return c.getInt(c.getColumnIndex(fieldName));
+    }
+
+    public static final long getLong(final Cursor c, final String fieldName) {
+        return c.getLong(c.getColumnIndex(fieldName));
+    }
+
+    public static final float getFloat(final Cursor c, final String fieldName) {
+        return c.getFloat(c.getColumnIndex(fieldName));
+    }
+}
