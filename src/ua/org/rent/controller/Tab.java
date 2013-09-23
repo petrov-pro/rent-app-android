dbdbc9a100db6d106d7e1cf85a892b00c3c69707
Andrey Petrov <petroff@favoritbet.com>
2013-09-23 13:05:41 +0300

    add TabActivity, Search, Result, History controller

---
+++ src/ua/org/rent/controller/Tab.java
@@ -0,0 +1,41 @@
+package ua.org.rent.controller;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import ua.org.rent.R;
+
+public class Tab extends TabActivity {
+
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// получаем TabHost
+		TabHost tabHost = getTabHost();
+
+		// инициализация была выполнена в getTabHost
+		// метод setup вызывать не нужно
+
+		TabHost.TabSpec tabSpec;
+
+		tabSpec = tabHost.newTabSpec("search");
+		tabSpec.setIndicator(getString(R.string.search), getResources().getDrawable(R.drawable.tab_icon_search));
+		tabSpec.setContent(new Intent(this, Search.class));
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("result");
+		tabSpec.setIndicator(getString(R.string.result), getResources().getDrawable(R.drawable.tab_icon_result));
+		tabSpec.setContent(new Intent(this, Result.class));
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("history");
+		tabSpec.setIndicator(getString(R.string.history), getResources().getDrawable(R.drawable.tab_icon_history));
+		tabSpec.setContent(new Intent(this, Result.class));
+		tabHost.addTab(tabSpec);
+	}
+}
--- src/ua/org/rent/settings/Consts.java
+++ src/ua/org/rent/settings/Consts.java
@@ -1,21 +1,13 @@
 package ua.org.rent.settings;
 
 /**
- * Created with IntelliJ IDEA.
- * User: admin
- * Date: 20.09.13
- * Time: 05:18
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: admin Date: 20.09.13 Time: 05:18 To change
+ * this template use File | Settings | File Templates.
  */
 public class Consts {
-    //Stub urls
-    public static final String URL_LOCAL_LOG="http://192.168.0.102";
+	//Stub urls
 
-    //valid url
-    public static final String HOST_API				= "http://pos.bth.in.ua/rent/";
-
-
-
-
-
+	public static final String URL_LOCAL_LOG = "http://192.168.0.102";
+	//valid url
+	public static final String HOST_API = "http://pos.bth.in.ua/rent/";
 }
